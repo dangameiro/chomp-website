2d994ee4110693dd96f4295c3e82a3c32e3b0ed0
Daniel Gameiro <gameiro@Daniels-MacBook-Pro.local>
2021-07-11 00:40:12 -0400

    Fixed random photos bug

--- scripts/random_photos.js
+++ scripts/random_photos.js
@@ -9,7 +9,7 @@ d3.json("photos.json").then(data => {
   document.getElementById("im2").setAttribute("src", photos[rand]);
   document.getElementById("im2link").setAttribute("href", links[rand]);
   photos.splice(rand, 1);
-  links.slice(rand, 1);
+  links.splice(rand, 1);
   rand = Math.floor(Math.random() * photos.length);
   document.getElementById("im3").setAttribute("src", photos[rand]);
   document.getElementById("im3link").setAttribute("href", links[rand]);
